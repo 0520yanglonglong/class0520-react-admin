9bc8648d5fc2a7027bbda2fdd92b35dcd4785110
longlong <15600122569@163.com>
2019-09-09 20:41:53 +0800

    qqqq

---
+++ src/redux/reducers.js
@@ -0,0 +1,13 @@
+/*
+* 根据prevState和action来生成newstate
+* */
+import { combineReducers } from 'redux';
+function xxx(prevstate ={},action) {
+    switch(action.type){
+        default:
+            return prevstate
+    }
+}
+export  default  combineReducers({
+    xxx
+})
